60c89a7991d0a011d452b521298406d72bd017df
HashedDan <georgedanielmangum@gmail.com>
2018-06-04 21:32:44 -0600

    using docs folder for github pages

---
+++ docs/path----9edf51937db6c8d1a323.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{103:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Daniel Mangum"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----9edf51937db6c8d1a323.js.map
