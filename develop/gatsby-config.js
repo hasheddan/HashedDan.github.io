1cc397f4051d342ceb7f7e544b2a8b9ea17d4573
HashedDan <georgedanielmangum@gmail.com>
2018-06-05 23:21:13 -0600

    added typography, page generation next

--- develop/gatsby-config.js
+++ develop/gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: 'Daniel Mangum',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
 }
